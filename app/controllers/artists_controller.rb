4cceffe6723cdd8dfd29dd18ccbfe35fba7f4655
Joseph Budina <joebud3@gmail.com>
2021-02-08 11:40:19 -0700

    Add form

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,12 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def new
+  end
+
+  def create
+    Artist.create(name: params[:name])
+    redirect_to '/artists'
+  end
 end
